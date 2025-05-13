4485536ef466ed18169fd79b05dbbdf6ae24e489
Pablo Boing Pscheidt <nikebryt@gmail.com>
2025-05-13 14:46:15 -0300

    feat: improve transaction fetching logic to handle server-side and client-side scenarios

--- lib/transactions.ts
+++ lib/transactions.ts
@@ -1,16 +1,24 @@
 import { Transaction } from "@/types/transaction";
+import fs from 'fs/promises'
+import path from 'path'
 
 export const ORIGIN = process.env.VERCEL_URL
     ? `https://${process.env.VERCEL_URL}`
     : 'http://localhost:3000';
 
 export async function fetchTransactions(): Promise<Transaction[]> {
+    if (typeof window === 'undefined') {
+        const filePath = path.join(process.cwd(), 'public', 'transactions.json')
+        const raw = await fs.readFile(filePath, 'utf-8')
+        return JSON.parse(raw) as Transaction[]
+    }
+
     const res = await fetch(`${ORIGIN}/transactions.json`, {
-        next: { 
+        next: {
             revalidate: 60,
             tags: ['transactions'],
         }
-      });
-      if (!res.ok) throw new Error('Failed to load transactions');
-      return res.json();
+    });
+    if (!res.ok) throw new Error('Failed to load transactions');
+    return res.json();
 }
